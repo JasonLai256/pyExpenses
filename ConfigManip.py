6de4c11806188bc5d366abace3ecb1ec0394a414
jason <jason@f3848131-1ccc-4c13-bcac-385d554e2296>
2011-12-24 14:12:25 +0000

    use JSON to store configuration file that insteat of ini format, and modify ConfigManip.py to compatible.
    git-svn-id: file:///home/jason/svn/expenses@12 f3848131-1ccc-4c13-bcac-385d554e2296

--- ConfigManip.py
+++ ConfigManip.py
@@ -1,38 +1,41 @@
 #! /usr/bin/env python
 # -*- coding:utf-8 -*-
 
-from ConfigParser import SafeConfigParser
+import json
 import sys
 import ErrorHandle as EH
 
-class ConfigManip:
+class ConfigManip(object):
     def __init__(self):
-        self.cp = SafeConfigParser()
         try:
-            self.cp.readfp(open('/home/jason/Py/Expenses/config.ini'))
+            self.obj = self._importObj()
         except IOError:
-            print 'file ./confi.ini not exist, can not initialise program.'
+            print "file ./confi.json not exist, can't initialise program."
             sys.exit(1)
 
+    def _importObj(self):
+        with open('/home/jason/Py/Expenses/config.json') as fil:
+            return json.load(fil)
+
+    def _exportObj(self):
+        with open('/home/jason/Py/Expenses/config.json', 'w') as fil:
+            json.dump(self.obj, fil)
+
     def secionts(self):
-        return self.cp.sections()
+        return self.obj.keys()
 
     def getInfo(self, option):
-        return self.cp.get('BaseInfo', option)
+        pass
             
     def setInfo(self, option, value):
         """Set the given option to the specifit value for BaseInfo section.
         @note: there are not to check the value is valid or not, so need
                to ensure value is valid before invoke this method.
         """
-        self.cp.set('BaseInfo', option, value)
+        pass
             
     def getOptions(self, section):
-        ret = []
-        opts = self.cp.options(section)
-        for item in opts:
-            ret.append(self.cp.get(section, item))
-        return ret
+        return self.obj.get(section, dict()).keys()
 
     def setOption(self, section, valtype, newval):
         """
@@ -60,40 +63,35 @@ class ConfigManip:
     def num2type(self, section, num):
         """Transform the Number to correctly maped Type.
         """
-        if not self.cp.has_section(section):
+        tnum = str(num)
+        if not self.obj.has_key(section):
             EH.valueError('No Section Error: {0}'.format(section))
-        
-        opts = self.cp.options(section)
-        if str(num) not in opts:
+            
+        if not self.obj[section].has_key(tnum):
             EH.valueError('{0} is not in the options'.format(num))
-        return self.cp.get(section, num)
+        return self.obj[section][tnum]
 
     def type2num(self, section, Type):
         """Transform the Type to correctly maped Number.
         """
-        if not self.cp.has_section(section):
+        Type = unicode(Type, encoding='UTF-8')
+        if not self.obj.has_key(section):
             EH.valueError('No Section Error: {0}'.format(section))
-        
-        opts = self.cp.options(section)
-        for item in opts:
-            if Type == self.cp.get(section, item):
-                return int(item)
+
+        for numkey in self.obj[section]:
+            if Type == self.obj[section][numkey]:
+                return int(numkey)
         else:
-            EH.valueError('{0} is not a valid type.'.format(consuType))
+            EH.valueError('{0} is not a valid type. {1}'.format(Type))
 
 
 if __name__ == '__main__':
     cm = ConfigManip()
-#    for item in cm.getConsumingType():
-#        print item,
-    print '\n\n'
-#    for item in cm.getPaymentType():
-#        print item,
     print cm.num2type('ConsumingType', '5')
     print cm.type2num('ConsumingType', '食物')
     print cm.num2type('PaymentType', '2')
     print cm.type2num('PaymentType', '现金')
-    print cm.num2type('ConsumingCatagory', '0')
-    print cm.type2num('ConsumingCatagory', '商务')
+    print cm.num2type('Tag', '0')
+    print cm.type2num('Tag', '商务')
     
     
