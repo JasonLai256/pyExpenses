0d5f124dfc81c58f102f2407e7be9b21ba17ef4d
Jason Lai <jasonlai256@gmail.com>
2012-02-09 11:34:24 +0800

    setup the __init__.py of pyExpenses module.

--- pyExpenses/__init__.py
+++ pyExpenses/__init__.py
@@ -1,1 +1,9 @@
-VERSION = (0, 0, 3)
+#!/usr/bin/env python
+"""Description ... """
+
+__version__ = "0.3"
+__author__ = [
+    "Jason Lai <jasonlai256@gmail.com>"
+]
+__license__ = ""
+__contributors__ = ""
